31d6bbea39485c021af9f75e25066728efc09566
Andrejchrcek <andrej.chrcek@gmail.com>
2025-12-02 11:21:50 +0100

    Update GPIO_Defs.hpp

--- include/GPIO_Defs.hpp
+++ include/GPIO_Defs.hpp
@@ -74,8 +74,11 @@ typedef enum
 #endif // def ARDUINO_ARCH_ESP8266
 
 // Platform specific GPIO definitions
+// Platform specific GPIO definitions
 #if   defined (BOARD_ESP32_CAM)
 #   include "platforms/GPIO_Defs_ESP32_CAM.hpp"
+#elif   defined (BOARD_ESP32_CAM_OV5640)
+#   include "platforms/GPIO_Defs_ESP32_CAM_OV5640.hpp"
 #elif defined (BOARD_ESP32_D1_MINI_ETH)
 #   include "platforms/GPIO_Defs_ESP32_D1_MINI_ETH.hpp"
 #elif defined (BOARD_ESP32_D1_MINI)
